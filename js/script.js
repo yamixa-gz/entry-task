5569d5683d35a69a0c9e2fd045c218a3c95c4fbc
yamixa-gz <yamixa.gz@gmail.com>
2021-01-12 22:17:23 +0200

    entry task are done.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const descriptionItems = document.querySelectorAll('div[data-show]')
+    const discussedItems = document.querySelectorAll('li[data-show]')
+    discussedItems.forEach( el => {
+                el.addEventListener('click', e => {
+                    const listAttrValue = e.target.getAttribute('data-show')  
+                    descriptionItems.forEach( el => {
+                        if ( listAttrValue === el.getAttribute('data-show') ) {
+                            el.classList.add('active')
+                        } else {
+                            el.classList.remove('active')
+                        }
+                    } )
+                    discussedItems.forEach( el => { el.classList.remove('active') } )                    
+                    e.target.classList.add('active') 
+                })    
+            })
+})
